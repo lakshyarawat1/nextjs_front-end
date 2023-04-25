ee65517cf1dc5b3c03d04e96b039e1fabb00070c
lakshyarawat1 <lakshya.paramount@gmail.com>
2023-04-25 22:44:27 +0530

    created explore section

--- client/sections/Hero.jsx
+++ client/sections/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => (
         alt="cover"
         className="w-full sm:h-[240px] h-[260px] object-cover rounded-tl-[140px] z-10 relative"
       />
-      <a href="explore">
+      <a href="#explore">
         <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
           <img
             src="/stamp.png"
